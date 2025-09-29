e2fe07b41b53a0f729d194644f9f69c00736c8ed
personaldemo001-spec <personaldemo001@gmail.com>
2025-09-29 10:18:49 +0530

    commit

--- firebase/client.ts
+++ firebase/client.ts
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCWkvI-sy8DPgyj3jgQAxVPW44zkTz3OYI",
-  authDomain: "prepwise-eca62.firebaseapp.com",
-  projectId: "prepwise-eca62",
-  storageBucket: "prepwise-eca62.firebasestorage.app",
-  messagingSenderId: "844118503236",
-  appId: "1:844118503236:web:221450baccaa210b43b111",
-  measurementId: "G-2N3Z1GD5GE",
+  apiKey: "AIzaSyCHATpiQPG0zJiappNM6GbNIJTssi64ldI",
+  authDomain: "prepwise-cff0c.firebaseapp.com",
+  projectId: "prepwise-cff0c",
+  storageBucket: "prepwise-cff0c.firebasestorage.app",
+  messagingSenderId: "429429163801",
+  appId: "1:429429163801:web:10934a84bff81a69046788",
+  measurementId: "G-0KYLYN11K1"
 };
 
 // Initialize Firebase
